6fd7d1f389f4feb393fb41bf1459a7e236d36602
Tim Menzies <timm@ieee.org>
2021-02-17 20:54:05 -0500

    saving

--- it.py
+++ it.py
@@ -1,3 +1,15 @@
+#i!/usr/bin/env python3
+# vim: ts=2 sw=2 sts=2 et tw=81:
+"""
+Very succinct object system that bundles nested functions and some get set
+methods into a container.
+
+- License: (c) 2021 Tim Menzies <timm@ieee.org>, MIT License  
+- Doco: http://menzies.us/it   
+- Code: http://github.com/timm/it
+- Example: see the [demoIt](#it.demoIt) function.
+
+"""
 from types import FunctionType as Fun
 
 class it:
@@ -14,3 +26,14 @@ class it:
       if type(v) == Fun and k[0] != "_": i.__dict__[k] = method(i,v)
     return i
 
+def itDemo()
+  from datetime import datetime as date
+  def Person(name=name,yob=1809):
+    def age(i): return date.now().year - i.yob
+    def birthday(i): i.weight *=1.05
+    return o(name=name, yob=yob,weight=100) + locals()
+  p = Person(name"Abraham")
+  for _ in range(56): p.birthday()
+  print(p,p.age())
+
+__name__ == "__main__" and itDemo()
