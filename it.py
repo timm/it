e2d5842d650c23ffa268105e80d35ebb78146dfb
Tim Menzies <timm@ieee.org>
2021-02-17 21:09:31 -0500

    saving

--- it.py
+++ it.py
@@ -15,11 +15,13 @@ from types import FunctionType as Fun
 class it:
   "Simple container of names fields, with methods."
   def __init__(i, **d): i.__dict__.update(d)
+
   def __repr__(i):
     "Pretty print, sorted keys, ignore private keys (those  with `_`)."
     return "{"+ ', '.join( 
       [f":{k} {v}" for k, v in sorted(i.__dict__.items())
        if  type(v) != Fun and k[0] != "_"])+"}"
+
   def __add__(i, maybe):
     "For all functions, add them as methods to `i`."
     def method(i,f): 
@@ -35,6 +37,7 @@ def itDemo():
     def age(i): return date.now().year - i.yob
     def birthday(i): i.weight = int(i.weight*1.05)
     return it(name=name, yob=yob,weight=100) + locals()
+
   #---------------------------
   p = Person(name="John")
   for _ in range(56): p.birthday()
